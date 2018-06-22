a733617d8392df404884ff0f3395f15f5f2625a3
volfeBR <rafael_hoffmann1205@hotmail.com>
2018-06-22 16:23:08 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,120 @@
+import java.util.*;
+public class main
+{
+    public static Random r         = new Random();
+    public static String playerName= "";            //Nome do jogador.
+    public static int linha[]      = new int[8];    //Array para preencher linhas corretamente.
+    public static boolean J1       = false;         //Seta o jogador atual.
+    public static boolean J2       = false;         //Seta o jogador atual.     
+    public static boolean PC       = false;         //Seta o jogador atual.
+    public static String lig4[][]  = new String[6][7];     //Matriz usada para jogar.
+    public static boolean hasWinner= false;                //Termina o jogo quando algum player ganhar.
+    public static void main(String[] args)
+    {
+        Scanner sc          = new Scanner(System.in);        
+        for(int i = 0; i<linha.length; i++)         //Preenche o array das linhas com o número da última linha.
+        {
+            linha[i] = 5;
+        }
+        
+        boolean wenttocatch = false;                //Usado para checar coluna.
+                
+        System.out.printf("Selecione o modo de jogo: \n     1 --- 2 Jogadores\n     2 --- Jogador Contra PC\n");
+        int modo = sc.nextInt();
+        switch(modo)
+        {
+            case 1:
+            startGame(1);
+            case 2:
+            startGame(2);
+            default:
+            startGame(1);
+        }
+           
+        while(!hasWinner)
+        {
+            do{
+                if(J1)
+                    playerName = "J1";
+                else if(J2)
+                    playerName = "J2";
+                else
+                    playerName = "PC";
+                
+                System.out.printf("É a vez do %s, digite a posição desejada\n", playerName);                
+                if(sc.hasNextInt()){
+                    int col = sc.nextInt();
+                    wenttocatch = true;
+                    play(col, lig4, modo);      
+                }else{
+                    sc.nextLine();
+                    System.out.println("Insira um valor de 1 à 7");
+                }
+            }while(!wenttocatch);
+        }
+    }
+    public static void startGame(int mode)
+    {            
+            if(mode == 1){
+                if(r.nextBoolean()){//Seleciona aleatório o primeiro jogador
+                J1 = true;
+                }else{
+                J2 = true;
+                }
+            }else if(mode == 2){ //Contra PC.
+                J1 = true;       //Sempre começa com jogador 1
+            }
+            reset(lig4);         //Limpa toda matriz.
+    }
+    public static void play(int col, String lig4[][], int modo)
+    {
+        if((col > 0 && col < 8) && linha[col-1] >= 0)
+        {
+            lig4[linha[col-1]][col-1] = playerName;        
+            linha[col-1]--;
+            print(lig4);
+            changePlayer(modo);
+        }else if(col <= 0 || col >= 8){
+            System.out.printf("Jogada Inválida, insira uma coluna de 1 à 7\n");
+        }else{
+            System.out.printf("Jogada Inválida, a coluna se encontra cheia\n");
+        }        
+    }
+    public static void changePlayer(int modo)
+    {
+        if(modo == 1){ //Contra outro Jogador
+            if(J1){
+            J1 = false;
+            J2 = true;
+            System.out.println(playerName);
+            }else if(!J1){
+            J2 = false;
+            J1 = true;
+            System.out.println(playerName);
+            }
+        }else if(modo == 2){ //Contra PC
+            
+        }
+    }
+    public static void reset(String lig4[][])
+    {
+        for(int i=0; i<lig4.length; i++)
+        {
+            for(int j = 0; j<lig4[0].length; j++)
+            {
+                lig4[i][j] = "";
+            }         
+        }
+    }
+    public static void print(String lig4[][])
+    {
+        for(int i=0; i<lig4.length; i++)
+        {
+            for(int j = 0; j<lig4[0].length; j++)
+            {                
+                System.out.printf(" |%2s|" ,lig4[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
